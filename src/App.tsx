061145abd4410fedff6536b94139cd17b7ee0a16
池田慎太郎 <ikeshi0015@icloud.com>
2023-03-19 19:47:53 +0900

    404ページ未実装

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { Button, ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 
 import { theme } from './theme/theme';
+import {Router} from "./router/Router"
 
 function App() {
   return (
     <div className="App">
       <ChakraProvider theme={theme}>
         <BrowserRouter>
-        <Button colorScheme={"teal"}>ボタン</Button>
-        <p>ああああああああああああああああ</p>
+          <Router />
         </BrowserRouter>
       </ChakraProvider>
     </div>
