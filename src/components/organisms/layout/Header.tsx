6df0831aa403210f82db32324a7216f20080eaa4
池田慎太郎 <ikeshi0015@icloud.com>
2023-04-08 13:49:44 +0900

    機能更新

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,36 +1,34 @@
 import { Box, Button, Drawer, DrawerBody, DrawerContent, DrawerOverlay, Flex, Heading, IconButton, Link, useDisclosure } from "@chakra-ui/react";
-import { memo, VFC } from "react";
-import { HamburgerIcon } from "@chakra-ui/icons";
+import { memo, useCallback, VFC } from "react";
+import { useHistory } from "react-router-dom";
+import { MenuIconButom } from "../../atoms/buttom/MenuIconButtom";
+import { MenuDrower } from "../../molecules/MenuDrower";
 
 export const Header: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const history = useHistory();
+
+  const onClickHome = useCallback(() => history.push("/home"), []);
+  const onClickUserManagement = useCallback(() => history.push("/home/user_management"), []);
+  const onClickSetting = useCallback(() => history.push("/home/Setting"), []);
+
   return (
     <>
       <Flex as="nav" bg="teal.500" color="gray.50" align="center" justify="space-between" padding={{ base: 3, md: 5 }}>
-        <Flex align={"center"} as="a" mr={8} _hover={{ cursor: "pointer" }}>
+        <Flex align={"center"} as="a" mr={8} _hover={{ cursor: "pointer" }} onClick={onClickHome}>
           <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
             ユーザー管理アプリ
           </Heading>
         </Flex>
         <Flex align={"center"} fontSize="sm" flexGrow={2} display={{ base: "none", md: "flex" }}>
           <Box pr={4}>
-            <Link>ユーザー一覧</Link>
+            <Link onClick={onClickUserManagement}>ユーザー一覧</Link>
           </Box>
-          <Link>設定</Link>
+          <Link onClick={onClickSetting}>設定</Link>
         </Flex>
-        <IconButton aria-label="メニューボタン" icon={<HamburgerIcon />} size="sm" variant={"unstyled"} display={{ base: "block", md: "none" }} onClick={onOpen} />
+        <MenuIconButom onOpen={onOpen} />
       </Flex>
-      <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
-        <DrawerOverlay>
-          <DrawerContent>
-            <DrawerBody p={0} bg="gray.100">
-              <Button w="100%">TOP</Button>
-              <Button w="100%">ユーザー一覧</Button>
-              <Button w="100%">設定</Button>
-            </DrawerBody>
-          </DrawerContent>
-        </DrawerOverlay>
-      </Drawer>
+      <MenuDrower onClose={onClose} isOpen={isOpen} onClickHome={onClickHome} onClickUserManagement={onClickUserManagement} onClickSetting={onClickSetting} />
     </>
   );
 });
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,26 @@
-import { FC, memo } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { FC, memo, useEffect } from "react";
+import { UserCard } from "../organisms/user/userCard";
+import { useAllusers } from "../../hooks/useAllusers";
 
-export const UserManagement :FC = memo(() => {
-    return <p>ユーザー管理ページです。</p>
-})
+export const UserManagement: FC = memo(() => {
+  const { getUsers, users, loading } = useAllusers();
+  useEffect(() => getUsers(), []);
+  return (
+    <>
+      {loading ? (
+        <Center h={"100vh"}>
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: "4", md: "10" }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx={"auto"}>
+              <UserCard imageURL="https://source.unsplash.com/random" userName={user.username} name={user.name} />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
+  );
+});
